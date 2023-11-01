3b6189a4a8c09fb5fd145f0234a86ca7aed36a6d
Alex Martinefsky <129073991+alexfandelcafe@users.noreply.github.com>
2023-10-31 23:25:23 -0300

    Add files via upload

---
+++ calculadora/funciones/script.js
@@ -0,0 +1,41 @@
+
+function numeroaletra(){
+
+  if(num==1){
+document.write("Uno");
+
+  }else{
+
+    if(num==2){
+
+        document.write("Dos");
+
+    }else{
+  
+        if(num==3){
+
+            document.write("Tres");
+
+        }else{
+      
+            if(num==4){
+
+                document.write("Cuatro");
+                
+            }else{
+          
+                if(num==5){
+                    document.write("Cinco");
+
+                }else{
+              
+              
+                  
+                } 
+            } 
+        }
+    }
+  }
+}
+
+let num= numeroaletra(num);
